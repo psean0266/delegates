5c36c3c19b514748d15adf1ce221609d4388a9b8
AbdoulayePangnonKOUL <AbdoulayePangnonKOUL@TAL-7G6S793>
2023-12-01 19:16:07 +0100

    dele

---
+++ delegates/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace delegates
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            // Demander le nom de l'utilisateur 
+            // Demander le numero de telephone de l'utilisateur
+
+            string nom = DemanderChaineUtilisateur("Quel est votre nom");
+
+            // 0610101010
+            string tel = DemanderChaineUtilisateur("Quel est votre numero de téléphone ?");
+
+            Console.WriteLine();
+            Console.WriteLine("Bonjour "+nom+ " , vous êtes joignable au" + tel);
+        }
+
+        static string DemanderChaineUtilisateur(string message)
+        {
+            Console.WriteLine(message);
+            string response = Console.ReadLine();   
+            return response;
+        }
+    }
+}
